070bd81d7b1f9606f52f7720bafeabf38ed752a1
Roshita <roshitha.23it@licet.ac.in>
2021-11-01 20:12:08 +0530

    add firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDYWjzXg1TsC0lXtKQkGeep_IMMl7z_onQ",
+  authDomain: "bothub-6a2e3.firebaseapp.com",
+  projectId: "bothub-6a2e3",
+  storageBucket: "bothub-6a2e3.appspot.com",
+  messagingSenderId: "649455413731",
+  appId: "1:649455413731:web:c60c68942c6b830c23d8d6",
+  measurementId: "G-9TVY77QJ8T"
+};
+
+const app = initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
