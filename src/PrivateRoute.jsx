0d5bc031a906817bc682c8f1e4e7ea68ccee4960
Roshita <roshitha.23it@licet.ac.in>
2021-11-03 23:26:52 +0530

    set Privateroute

--- src/PrivateRoute.jsx
+++ src/PrivateRoute.jsx
@@ -1,53 +1,66 @@
 import React from "react";
 import { Redirect } from "react-router-dom";
-// import firebase from "firebase/app";
-// TODO: REWRITE
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import { Spinner } from "react-bootstrap";
+import { routes } from "./App";
 
-class PrivateRoute extends React.Component {
-    constructor(props){
+
+function logout() {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        console.log("Signed out")
+      }).catch((error) => {
+        console.log(error.message)
+    });
+    window.location.reload();
+}
+export default class PrivateRoute extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            isLoaded:false,
+        this.state = {
+            isLoaded: false,
+            user: false
         };
     }
 
-// componentDidMount() {
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         if(user){
-//             this.setState({
-//                 isLoaded:true,
-//                 user:user
-//             });
-//         }else{
-//             this.setState({
-//                 isLoaded:true,
-//                 user:false
-//             });
-//         }
-//     }.bind(this));
-// }
+    componentDidMount() {
+        const auth = getAuth();
+         onAuthStateChanged(auth, (user) => {
+            if (user) {
+                console.log("AuthState: True")
+                this.setState({
+                    user,
+                    isLoaded: true
+                })
+            } else {
+                console.log("AuthState: False")
+            }
+        })
 
-// render() {
-//     const{isLoaded,user}=this.state;
+    }
 
-//     if(!isLoaded){
-//         return(
-//             <div className="d-flex justify-content-center align-items-center"
-//                     style={{ height: "100vh", width: "100vw" }}>
-            
-//             <LinearProgress className="w-25"></LinearProgress>
+    render() {
+        const {isLoaded, user} = this.state;
 
-//          </div>
-//         );
-//     }else{
-//         if(user){
-//             const Component= this.props.component;
-//             return <Component/>
-//         }else{
-//             return <Redirect to={{pathname:"/"}} />;
-//         }
-//     }
-// }
+        if (!isLoaded) {
+            return (
+                <div className="d-flex justify-content-center align-items-center"
+                        style={{ height: "100vh", width: "100vw" }}>
+                    <Spinner animation="border" />
+                </div>
+            );
+
+        } else {
+
+            const Component = this.props.component;
+
+            if (user) {
+                return <Component login={true} user={user}/>
+            } else if (this.props.shouldLogin === true) {
+                return <Redirect to={{pathname: routes.login}}/>;
+            } else {
+                return <Component login={false}/>
+            }
+        }
+    }
 }
-
-export default PrivateRoute;
