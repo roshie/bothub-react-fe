a95684982f76e5f56285d1f2db741fa382b51aa0
Roshita <roshitha.23it@licet.ac.in>
2021-11-04 00:33:40 +0530

    init components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,34 @@
 import Layout from "./components/Layout"
 
 
+function LandingPage() {
+    return (
+        <section className="vh-100">
+            <div className="row m-auto">
+                <div className="col-12 col-md-6">
+                    <div className="fw-bold text-light tagline">A Shop for all Automation Projects</div>
+                </div>
+                <div className="col-12 col-md-6"></div>
+            </div>
+        </section>
+    );
+}
+
+function Categories() {
+    return (
+        <section className="vh-100 text-light">
+            <div className="row m-auto">
+                Categories
+            </div>
+        </section>
+    );
+}
+
 export default function Home(props) {
     return (
         <Layout loginState={props.login} page="home">
-            <h1 className="text-dark"> Home</h1>
+            <LandingPage />
+            <Categories />
         </Layout>
     );
 }
