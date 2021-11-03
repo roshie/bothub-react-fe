fbe162b8fca585db3c0fa3941e004e8a912d2f53
Roshita <roshitha.23it@licet.ac.in>
2021-11-03 23:27:44 +0530

    init Home and login pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,10 @@
+import Layout from "./components/Layout"
 
 
-
-export default function Home() {
-    return (<h1 className="text-dark"> Home</h1>)
+export default function Home(props) {
+    return (
+        <Layout loginState={props.login} page="home">
+            <h1 className="text-dark"> Home</h1>
+        </Layout>
+    );
 }
