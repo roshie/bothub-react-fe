2b6552d88eabb4db86dea11cc1ce9925fd909f20
Roshita <roshitha.23it@licet.ac.in>
2021-11-06 14:47:16 +0530

    fixes

---
+++ src/config.js
@@ -0,0 +1,11 @@
+
+
+// Localhost
+export const customerAppUrl = "http://localhost:3000"
+export const adminAppUrl = ""
+export const backendAppUrl = "http://localhost:8000"
+
+// Development
+// export const customerAppUrl = ""
+// export const adminAppUrl = "https://bothub.vercel.app"
+// export const backendAppUrl = "https://bothub-be.herokuapp.com"
