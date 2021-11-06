50cba56d4a365315d1cfd998b3fc683f070a99dc
Kingswin <kingswin5549@gmail.com>
2021-11-06 23:57:06 +0530

    landing page and 3d printing page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,16 @@ import { useEffect } from "react";
 function LandingPage(props) {
   return (
     <section className="vh-100" id="home" name="home">
-      <div className="row m-auto">
-        <div className="col-12 col-md-6">
-          <div className="fw-bold text-light tagline">
-            A Shop for all Automation Projects
-          </div>
-        </div>
-        <div className="col-12 col-md-6"></div>
+      <div className="hero d-flex justify-content-center align-items-center cen">
+        <div className="row w-75">
+          <div className="col-lg-7 col-md-12 col-sm-12 col-xs-12 mt-md-5 padding_zero d-flex justify-content-center flex-column">
+              <div className="fw-bold text-light tagline">A Shop for all Automation Projects.</div>
+              <a href="#"><button id="Explore" className="btn-lg btn btn-secondary fw-bold my-2">Explore</button></a>
+            </div>  
+            <div className="col-lg-5 col-md-6 col-sm-12 col-xs-12 my-auto padding_zero text-center">
+                <img className="pic floating" src="ROBOT.png"/>
+          </div>     
+        </div>   
       </div>
     </section>
   );
@@ -44,7 +47,19 @@ function D3Printing(props) {
         className="vh-100 text-light"
         name="threeDPrinting"
       >
-        <div className="row m-auto">3d Printing</div>
+        <div className="hero d-flex justify-content-center align-items-center cen">
+          <div className="row w-75">
+            <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12 padding_zero d-flex justify-content-center flex-column">
+                <div className="fw-bold text-light mb-2 mt-4" style={{fontSize: '25px'}}>3D Printing</div>
+                <div className="fw-bold text-light tagline">You Design, We Develope</div>
+                <div className="fw-bold text-light mb-2" style={{fontSize: '20px'}}>We provied 3D Printing services</div>
+                <a href="#"><button id="View" className="btn-lg btn btn-secondary fw-bold my-2">View Details</button></a>
+            </div>  
+            <div className="col-lg-7 col-md-6 col-sm-12 col-xs-12 my-auto padding_zero text-center">
+                <img className="pict"src="3d-printer.png"/>
+            </div>     
+          </div>   
+        </div>
       </section>
     );
 }
