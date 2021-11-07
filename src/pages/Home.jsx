243170a9587293c93779bedde667e5c40c857a33
Roshita <roshitha.23it@licet.ac.in>
2021-11-07 15:00:09 +0530

    init carousel space

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,18 @@ import { useEffect } from "react";
 
 function LandingPage(props) {
   return (
-    <section className="vh-100" id="home" name="home">
-      <div className="hero d-flex justify-content-center align-items-center cen">
-        <div className="row w-75">
-          <div className="col-lg-7 col-md-12 col-sm-12 col-xs-12 mt-md-5 padding_zero d-flex justify-content-center flex-column">
-              <div className="fw-bold text-light tagline">A Shop for all Automation Projects.</div>
-              <a href="#"><button id="Explore" className="btn-lg btn btn-secondary fw-bold my-2">Explore</button></a>
+    <section className="vh-100 d-flex justify-content-center align-items-center" id="home" name="home">
+      <div className="mt-neg-5" id="bg-carousel">
+        <div className="row w-100 h-100 mx-auto">
+            <div className="col-12 col-lg-6 d-flex justify-content-center flex-column ps-lg-5 px-5 px-lg-0">
+                <div className="fw-bold text-light tagline text-center text-lg-start mx-0 ms-lg-5">A Stop for all Automation Devices.</div>
+                <div className="text-center text-lg-start mx-0 ms-lg-5">
+                    <a href="#categories" className="btn-lg btn btn-secondary fw-bold my-2 fs-5">Explore</a>
+                </div>
             </div>  
-            <div className="col-lg-5 col-md-6 col-sm-12 col-xs-12 my-auto padding_zero text-center">
-                <img className="pic floating" src="ROBOT.png"/>
-          </div>     
+            <div className="col-12 col-lg-6 my-auto text-center d-none d-lg-block">
+                {/* Todo: carousel */}
+            </div>     
         </div>   
       </div>
     </section>
@@ -22,7 +24,7 @@ function LandingPage(props) {
 
 function Categories(props) {
   return (
-    <section id="categories" className=" text-light" name="categories">
+    <section id="categories" className="text-light" name="categories">
         <div className="row m-auto w-75">
             <div
                 className="text-center pt-5 h2 "
@@ -56,7 +58,7 @@ function D3Printing(props) {
                 <a href="#"><button id="View" className="btn-lg btn btn-secondary fw-bold my-2">View Details</button></a>
             </div>  
             <div className="col-lg-7 col-md-6 col-sm-12 col-xs-12 my-auto padding_zero text-center">
-                <img className="pict"src="3d-printer.png"/>
+                {/* <img className="pict"src="3d-printer.png"/> */}
             </div>     
           </div>   
         </div>
@@ -66,7 +68,7 @@ function D3Printing(props) {
 
 export default function Home(props) {
     useEffect(() => {
-        document.title = "Bothub | A Stop for all Automation Projects"
+        document.title = "Bothub | A Stop for all Automation Devices"
 
         // eslint-disable-next-line
     }, [])
