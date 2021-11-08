4681c99753170263d43b1a8cbc1003166eb235d7
Roshita <roshitha.23it@licet.ac.in>
2021-11-08 23:28:17 +0530

    error handlingss

--- src/pages/Authenticate.jsx
+++ src/pages/Authenticate.jsx
@@ -1,12 +1,12 @@
 import { Redirect } from "react-router";
 import { routes } from "../App";
 import { useEffect, useState } from "react";
-import { FloatingLabel, Form, Button, Col } from "react-bootstrap";
+import { FloatingLabel, Form, Button, Col, Spinner } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { Link } from "react-router-dom";
 import { DecorElemBorder, DecorElemBox } from "./components/decorElements";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword } from "firebase/auth"
 import { backendAppUrl } from '../config'
 
 
@@ -17,6 +17,7 @@ export default function Authenticate(props) {
     var [email, setEmail] = useState('')
     var [password, setPassword] = useState('')
     var [errorMsg, setErrorMsg] = useState('')
+    var [loading, setLoading] = useState(false)
 
     useEffect(() => {
         document.title = `${props.page === "login"? "Login" : "Sign Up"} | Bothub`
@@ -31,18 +32,27 @@ export default function Authenticate(props) {
             },
             body: JSON.stringify(data),
         })
-        .then(res => {
-            res = res.json()
-            if (res.detail == "exists") {
-                setErrorMsg('An account with this EmailID already exists. Try Logging in.')
-            } else if (res.detail == "success") {
-                // remove loader
-                window.location.href = '/' // Redirect to user details
-                console.log("Email-not-verified")
-            }
-        })
-        .catch(err => {
+        .then((response) => {
+            if (response.ok) {
+                const result = response.json();
+                if (result.detail == "exists") {
+                    setErrorMsg('An account with this EmailID already exists. Try Logging in.')
+                    setLoading(false)
+                } else if (result.detail == "success") {
+                    // remove loader
+                    setLoading(false)
+                    movePage(false)
+                }
+              } else {
+                console.error(response.json().detail);
+                setErrorMsg('There was a problem while signing up. Please try again')
+                setLoading(false)
+              }
+        },
+        (err) => {
             console.log(err)
+            setErrorMsg('There was a problem while signing up. Please try again')
+            setLoading(false)
         })
     }
 
@@ -54,26 +64,28 @@ export default function Authenticate(props) {
             .then((result) => {
                 localStorage.uid = result.user.uid;
                 if (props.page !== 'login') {
+                    result.user.emailVerified = false
                     const data = {
                         signUpMethod: 'google',
                         email,
                         uid: result.user.uid
                     }
+                    setLoading(true)
                     signUp(data)
                 }
-                else if (!result.user.emailVerified) {
-                    window.location.href = '/' // Redirect to user details
-                    console.log("Email-not-verified")
+                else {
+                    setLoading(false)
+                    movePage(result.user.emailVerified)
                 }
-                else if (result.user.emailVerified)
-                    window.location.href = (redirectTo !== null) ?  routes[redirectTo] : '/'
             }).catch((error) => {
                 console.log(error.message)
-        });
+                setLoading(false)
+            });
     }
 
     const submitButton = () => {
         setErrorMsg('')
+        setLoading(true)
         if (props.page != 'login') {
             const data = {
                 signUpMethod: 'custom',
@@ -82,8 +94,31 @@ export default function Authenticate(props) {
             }
             signUp(data)
         } else {
-            // password login code
+            const auth = getAuth();
+            signInWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                setLoading(false)
+                movePage(user.emailVerified)
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.error(errorMessage)
+                setLoading(false)
+                setErrorMsg(errorMessage)
+            });
         }
+    }
+
+    const movePage = (emailVerified) => {
+        if (!emailVerified) {
+            window.location.href = '/' // Redirect to user details
+            console.log("Email-not-verified")
+        }
+        else if (emailVerified)
+            window.location.href = (redirectTo !== null) ?  routes[redirectTo] : '/'
     }
     
     return (
@@ -112,18 +147,18 @@ export default function Authenticate(props) {
                                 </FloatingLabel>
                                 <FloatingLabel controlId="floatingPassword" label="Password"className="mb-3 text-light">
                                     <Form.Control className="bg-primary border-primary text-light" required value={password} onChange={(e) => {setPassword(e.target.value)}} type="password" placeholder="Password" />
-                                    <Form.Text id="passwordHelpBlock" className="text-danger" muted> {errorMsg} </Form.Text>
+                                    <Form.Text className="text-danger" muted> {errorMsg} </Form.Text>
                                 </FloatingLabel>
                                 <Col className="text-center mt-4">
-                                    <Button variant="secondary" size="lg" type="submit" onClick={submitButton}>
-                                        { props.page === "login"? "Login" : "Sign Up" }
+                                    <Button variant="secondary" size="lg" type="submit" disabled={loading} onClick={submitButton}>
+                                        {loading ? <Spinner animation="border" size="sm" className="text-light"/> : (props.page === "login") ? "Login" : "Sign Up" }
                                     </Button>
                                 </Col>
                             </Form>
                             <hr className="bg-primary" style={{height: '3px'}}/>
                             <Col className="text-center mb-4">
                                 <div className="fs-6">Or, { props.page === "login" ? "Login" : "Sign Up" } with</div>
-                                <Button variant="secondary" onClick={signInwithGoogle} className="my-3" type="submit"> <FontAwesomeIcon icon={faGoogle}/> Google </Button>
+                                <Button variant="secondary" onClick={signInwithGoogle} disabled={loading} className="my-3" type="submit"> <FontAwesomeIcon icon={faGoogle}/> Google </Button>
                                 { props.page === "login" ? 
                                     <div>Don't Have an Account? <Link to="/sign-up" className="text-decoration-none text-secondary">Sign Up</Link></div>
                                 : 
