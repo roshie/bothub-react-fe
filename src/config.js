191b33816eb842a1006c7322221d112a70981e5b
Roshita <67852344+roshie@users.noreply.github.com>
2022-01-20 14:49:07 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -14,6 +14,7 @@ export const backendAppUrl = "https://bothub-be.herokuapp.com"
 export function getRequestParams(method, data) {
     return {
         method, 
+        mode: 'cors',
         headers: {
         'Content-Type': 'application/json'
         },
