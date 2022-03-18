3e934df024b8f5317f21dbe4747f24fbe9bf204a
Kingswin <kingswin5549@gmail.com>
2022-03-18 15:37:47 +0530

    carousel

--- src/pages/threeDPrinting.jsx
+++ src/pages/threeDPrinting.jsx
@@ -3,6 +3,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import {Form, FloatingLabel, Row} from "react-bootstrap";
 
+
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import "swiper/swiper.min.css";
+// import "swiper/components/effect-coverflow/effect-coverflow.min.css";
+// import "swiper/components/pagination/pagination.min.css";
+// import "swiper/components/navigation/navigation.min.css";
+// import "./styles.css";
+// import SwiperCore, {
+//   EffectCoverflow,
+//   Pagination,
+//   Navigation
+// } from "swiper/core";
+
+// SwiperCore.use([EffectCoverflow, Pagination, Navigation]);
+
+
+// import img1 from "../assets/slideshow-1.jpg";
+// import img2 from "../assets/slideshow-2.jpg";
+// import img3 from "../assets/slideshow-3.jpg";
+// import img4 from "../assets/slideshow-4.jpg";
+
+
+
 export default function ThreeDPrinting(props) {
   return (
     <Layout
@@ -103,9 +126,59 @@ export default function ThreeDPrinting(props) {
         </div>
       </div>
       </section>
-      <section className="">
-        
-      </section>
+
+    <section className="vh-100" id="home" name="home">
+    <div className="row m-auto w-75">
+       {/* <div className="container">
+      <div className="title_wrapper">
+        <div className="reactLogo">
+          <img src="images/react.png" />
+        </div>
+        <div className="title_">
+          <span>React</span>Swiper Slider
+        </div>
+      </div>
+      <Swiper
+        navigation={true}
+        effect={"coverflow"}
+        centeredSlides={true}
+        slidesPerView={window.innerWidth < 768 ? 1 : "auto"}
+        loop={true}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true
+        }}
+        pagination={{
+          clickable: true
+        }}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="images/1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="images/6.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </div> */}
+    </div>
+</section>
     </Layout>
+    
   );
 }
