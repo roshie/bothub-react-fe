884e4dc511526a952b9d72f20991389a725359c7
Roshita <67852344+roshie@users.noreply.github.com>
2022-04-13 19:14:54 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 
 
 // Localhost
-export const customerAppUrl = "http://localhost:3000"
-export const adminAppUrl = ""
-export const backendAppUrl = "http://localhost:8000"
+// export const customerAppUrl = "http://localhost:3000"
+// export const adminAppUrl = ""
+// export const backendAppUrl = "http://localhost:8000"
 
 // Development
-// export const customerAppUrl = ""
-// export const adminAppUrl = "https://bothub.vercel.app"
-// export const backendAppUrl = "https://bothub-be.herokuapp.com"
+export const customerAppUrl = "https://bothub.vercel.app"
+export const adminAppUrl = ""
+export const backendAppUrl = "https://bothub-be.herokuapp.com"
 
 
 export function getRequestParams(method, data) {
